d82699fc4e398e558774cca0ff840f8ad1dcd84a
Enyewashebr <Enyewashebir0@gmail.com>
2024-10-24 00:46:57 +0300

    I write codes that calculate personal finance

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,8 @@
+monthly_income = float(input("Enter your monthly income: "))
+total_monthly_expenses = float(input("Enter your total monthlyexpenses: "))
+monthly_saving = monthly_income-total_monthly_expenses
+rate = 0.05
+projected_saving = monthly_saving * 12 + monthly_saving * 12 * 0.05
+
+print("Your monthly savings are", monthly_saving)
+print("Projected savings after one year, with interest, is:", projected_saving)
